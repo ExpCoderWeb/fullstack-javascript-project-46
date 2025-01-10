22a5cb72cf03fa9981ca7af0645ea974211b573a
ExpCoderWeb <shagaev_2011@mail.ru>
2025-01-10 16:37:02 +0400

    add src/getDiff.js

---
+++ src/getDiff.js
@@ -0,0 +1,29 @@
+import _ from 'lodash';
+
+const getDiff = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const uniqueKeys = _.union(keys1, keys2);
+  const sortedKeys = _.sortBy(uniqueKeys);
+
+  const properties = sortedKeys
+    .map((sortedKey) => {
+      if (!Object.hasOwn(data1, sortedKey)) { 
+        return { key: sortedKey, value: data2[sortedKey], type: 'added' };
+      }
+
+      if (!Object.hasOwn(data2, sortedKey)) { 
+        return { key: sortedKey, value: data1[sortedKey], type: 'deleted' };
+      }
+
+      if(data1[sortedKey] !== data2[sortedKey]) {
+        return { key: sortedKey, oldValue: data1[sortedKey], newValue: data2[sortedKey], type: 'changed' };
+      }
+
+      return { key: sortedKey, value: data1[sortedKey], type: 'unchanged' };
+    });
+
+  return properties;
+};
+
+export default getDiff;
