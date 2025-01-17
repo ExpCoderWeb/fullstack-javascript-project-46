cce6539e2b5a7d2c34f6d068f0323c2f66e785c9
ExpCoderWeb <shagaev_2011@mail.ru>
2025-01-17 18:08:48 +0400

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 import parseFile from './parsers.js';
 import getDiff from './getDiff.js';
-import stylish from './formatters/stylish.js';
+import getFormatter from './formatters/index.js';
 
 const readFile = (filepath) => fs.readFileSync(filepath, 'utf-8');
 
@@ -10,20 +10,21 @@ const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const getExtension = (filepath) => path.extname(filepath);
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const readFile1 = readFile(getFullPath(filepath1));
   const readFile2 = readFile(getFullPath(filepath2));
 
   const extensionFile1 = getExtension(filepath1);
   const extensionFile2 = getExtension(filepath2);
 
-  const parsedFile1 = parseFile(readFile1, extensionFile1);
-  const parsedFile2 = parseFile(readFile2, extensionFile2);
+  const parsedFile1 = parseFile(readFile1, extensionFile1) || {};
+  const parsedFile2 = parseFile(readFile2, extensionFile2) || {};
 
-  const filesDiff = getDiff(parsedFile1, parsedFile2);
-  const formatDiff = stylish(filesDiff);
+  const diff = getDiff(parsedFile1, parsedFile2);
 
-  return formatDiff;
+  const formattedDiff = getFormatter(formatName)(diff);
+
+  return formattedDiff;
 };
 
 export default genDiff;
