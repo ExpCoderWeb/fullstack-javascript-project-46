5ac444268cb0d322fd09762dc4d163bfcd48e342
ExpCoderWeb <shagaev_2011@mail.ru>
2025-01-17 18:14:36 +0400

    update stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,30 +1,51 @@
-const stylish = (tree) => {
-  const replacer = ' ';
-  const spacesCount = 2;
+import _ from 'lodash';
 
-  const indentSize = spacesCount;
-  const currentIndent = replacer.repeat(indentSize);
-  const closingBracketIndent = replacer.repeat(indentSize - spacesCount);
+const stylish = (value, replacer = ' ', spacesCount = 4) => {
+  const iter = (currentValue, depth) => {
+    if (!_.isObject(currentValue)) {
+      return `${currentValue}`;
+    }
 
-  const properties = tree
-    .map((property) => {
-      switch (property.type) {
-        case 'added':
-          return `${currentIndent}+ ${property.key}: ${property.value}`;
-        case 'deleted':
-          return `${currentIndent}- ${property.key}: ${property.value}`;
-        case 'changed':
-          return `${currentIndent}- ${property.key}: ${property.oldValue}\n${currentIndent}+ ${property.key}: ${property.newValue}`;
-        default:
-          return `${currentIndent}  ${property.key}: ${property.value}`;
-      }
-    });
+    const indentSize = depth * spacesCount;
+    const currentIndent = replacer.repeat(indentSize - 2);
+    const closingBracketIndent = replacer.repeat(indentSize - spacesCount);
 
-  return [
-    '{',
-    ...properties,
-    `${closingBracketIndent}}`,
-  ].join('\n');
+    if (Array.isArray(currentValue)) {
+      const properties = currentValue
+        .map((property) => {
+          switch (property.type) {
+            case 'added':
+              return `${currentIndent}+ ${property.key}: ${iter(property.value, depth + 1)}`;
+            case 'deleted':
+              return `${currentIndent}- ${property.key}: ${iter(property.value, depth + 1)}`;
+            case 'changed':
+              return `${currentIndent}- ${property.key}: ${iter(property.oldValue, depth + 1)}\n${currentIndent}+ ${property.key}: ${iter(property.newValue, depth + 1)}`;
+            case 'unchanged':
+              return `${currentIndent}  ${property.key}: ${iter(property.value, depth + 1)}`;
+            default:
+              return `${currentIndent}  ${property.key}: ${iter(property.children, depth + 1)}`;
+          }
+        });
+
+      return [
+        '{',
+        ...properties,
+        `${closingBracketIndent}}`,
+      ].join('\n');
+    }
+
+    const props = Object
+      .entries(currentValue)
+      .map(([key, val]) => `${currentIndent}  ${key}: ${iter(val, depth + 1)}`);
+
+    return [
+      '{',
+      ...props,
+      `${closingBracketIndent}}`,
+    ].join('\n');
+  };
+
+  return iter(value, 1);
 };
 
 export default stylish;
