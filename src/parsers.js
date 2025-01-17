f1cfc911512bf7776ebdd391e44cd2475f26c699
ExpCoderWeb <shagaev_2011@mail.ru>
2025-01-17 18:09:22 +0400

    update src/parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,11 +1,14 @@
+import yaml from 'js-yaml';
+
 const parseFile = (data, extension) => {
   switch (extension) {
     case '.json':
       return JSON.parse(data);
     case '.yml':
-      return;
     case '.yaml':
-      return;
+      return yaml.load(data);
+    default:
+      throw new Error(`Invalid file extension - ${extension}`);
   }
 };
 
