189051a7e71ced7e7b4b2f25e4a4ac5d165a22ae
ExpCoderWeb <shagaev_2011@mail.ru>
2025-01-23 19:06:04 +0400

    modify bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format', 'stylish')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .helpOption('-h, --help', 'output usage information')
   .action((filepath1, filepath2) => {
     const formatName = program.opts().format;
